3dcaaee7aa2a3f4c3547f62dd8fe19294caad756
phanphuc2210 <phucpth2001@gmail.com>
2022-08-31 18:05:15 +0700

    Create menu table & an interface to add categories

---
+++ resources/views/admin/menu/add.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.main')
+@section('head')
+    <script src="/ckeditor/ckeditor.js"></script>
+@endsection
+
+@section('content')
+<form action="" method="POST">
+    <div class="card-body">
+      <div class="form-group">
+        <label for="name">Tên danh mục</label>
+        <input type="text" name="name" class="form-control" id="name" placeholder="Enter name">
+      </div>
+
+      <div class="form-group">
+        <label for="">Danh mục</label>
+        <select name="parent_id" class="form-control" >
+            <option value="0">Danh mục cha</option>
+        </select>
+      </div>
+      
+      <div class="form-group">
+        <label for="description">Mô tả</label>
+        <textarea name="description" class="form-control" ></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="content">Mô tả chi tiết</label>
+        <textarea name="content" id="content" class="form-control" ></textarea>
+      </div>
+
+      <div class="form-group">
+        <label>Kich hoạt</label>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" value="1" type="radio" id="active" name="active" checked="">
+          <label for="active" class="custom-control-label">Có</label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" value="0" type="radio" id="no_active" name="active" >
+          <label for="no_active" class="custom-control-label">Không</label>
+        </div>
+      </div>
+    </div>
+    <!-- /.card-body -->
+
+    <div class="card-footer">
+      <button type="submit" class="btn btn-primary">Tạo danh mục</button>
+    </div>
+  </form>
+@endsection
+
+@section('footer')
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor 4
+        // instance, using default configuration.
+        CKEDITOR.replace( 'content' );
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +24,16 @@ Route::get('admin/users/login', [LoginController::class, 'index'])->name('login'
 Route::post('admin/users/login/store', [LoginController::class, 'store']);
 
 Route::middleware(['auth'])->group(function(){
-    Route::get('admin', [MainController::class, 'index'])->name('admin');
-    Route::get('admin/main', [MainController::class, 'index']);
+    Route::prefix('admin')->group(function(){
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::get('/main', [MainController::class, 'index']);
+
+        // Menu
+        Route::prefix('menus')->group(function(){
+            Route::get('/add', [MenuController::class, 'create']);
+        });
+    });
+
+    
 });
 
