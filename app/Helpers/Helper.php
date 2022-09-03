6a84bf68e740dc27834b49de6888217bfb3d0f4d
phanphuc2210 <phucpth2001@gmail.com>
2022-09-03 13:37:50 +0700

    Load the Menu to the interface

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -41,4 +41,36 @@ class Helper {
         return $active == 0? '<span class="btn btn-danger btn-xs">NO</span>' 
                     : '<span class="btn btn-success btn-xs">YES</span>';
     }
+
+    public static function menus($menus, $parent_id = 0){
+        $html = '';
+
+        foreach($menus as $key => $menu){
+            if($menu->parent_id == $parent_id){
+                $html .= '
+                    <li>
+                        <a href="/danh-muc/'. $menu->id .'-'. Str::slug($menu->name, '-') .'.html"> '. $menu->name .' </a>';
+                
+                if(self::isHasChild($menus, $menu->id)){
+                    $html .= '<ul class="sub-menu">';
+                    $html .= self::menus($menus, $menu->id);
+                    $html .= '</ul>';
+                }
+                
+                $html .= '</li>';
+            }
+        }
+
+        return $html;
+    }
+
+    public static function isHasChild($menus, $id) {
+        foreach($menus as  $menu){
+            if($menu->parent_id == $id){
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
